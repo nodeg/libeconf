a9dbb95e6240dd17a4c1707436e4649e994e9408
Dominik Gedon <dgedon@suse.de>
2019-09-16 16:42:27 +0200

    created new branch for new file listing function

--- util/econfctl.c
+++ util/econfctl.c
@@ -19,7 +19,6 @@
   SOFTWARE.
 */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -28,16 +27,9 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-
 #include "../include/libeconf.h"
 
-
-
-
 static void usage();
-
-
-
 
 int main (int argc, char *argv[]) {
 	econf_file *key_file = NULL;
@@ -61,43 +53,66 @@ int main (int argc, char *argv[]) {
 		char *suffix_array[2];
 
         while (suffix != NULL) {
-		    suffix_array[position++] = suffix;
-                suffix = strtok(NULL, ".");
+            suffix_array[position++] = suffix;
+            suffix = strtok(NULL, ".");
         }
+        free(suffix);
 		
-		/* process given configuration file directories */
-		if ((error = econf_readDirs(&key_file, "/usr/etc", "/etc", argv[2], suffix_array[1], "=", "#"))) {
+        /*
+         * process given configuration file directories. A wrapper has to be
+         * written in order to parse more directories. At this moment only
+         * /etc and /usr/etc is handled.
+         */
+        if ((error = econf_readDirs(&key_file, "/usr/etc", "/etc", suffix_array[0], suffix_array[1], "=", "#"))) {
 			fprintf(stderr, "%s\n", econf_errString(error));
-		}
+            econf_free(key_file);
+            return EXIT_FAILURE;
+        }
 
-		char **groups = NULL;
+        /* TODO: include econf_mergeFiles() */
+
+        char **groups = NULL;
         char **keys = NULL;
-		char *value = NULL;
+        char *value = NULL;
         size_t group_count = 0;
         size_t key_count = 0;
 
         /* show groups, keys and their value */
 		if ((error = econf_getGroups(key_file, &group_count, &groups))) {
 			fprintf(stderr, "%s\n", econf_errString(error));
+            econf_free(groups);
+            econf_free(keys);
+            econf_free(key_file);
+            free(value);
+            return EXIT_FAILURE;
 		}
 		for (size_t g = 0; g < group_count; g++) {
 			if ((error = econf_getKeys(key_file, groups[g], &key_count, &keys))) {
 				fprintf(stderr, "%s\n", econf_errString(error));
+                econf_free(groups);
+                econf_free(keys);
+                econf_free(key_file);
+                free(value);
+                return EXIT_FAILURE;
 			}
 			printf("%s\n", groups[g]);
 			for (size_t k = 0; k < key_count; k++) {                		
 				if ((error = econf_getStringValue(key_file, groups[g], keys[k], &value)) || value == NULL || strlen(value) == 0) {
 					fprintf(stderr, "%s\n", econf_errString(error));
+                    econf_free(groups);
+                    econf_free(keys);
+                    econf_free(key_file);
+                    free(value);
+                    return EXIT_FAILURE;
 				}
 				printf("%s = %s\n", keys[k], value);
 			}
 			printf("\n");
 		}
-		
         econf_free(groups);
 		econf_free(keys);
 		free(value);
-		free(suffix);
+
 
 	/* econfctl cat */
 	} else if (strcmp(argv[1], "cat") == 0) {
@@ -133,6 +148,8 @@ int main (int argc, char *argv[]) {
                 /* if no editor is specified take vim as default */
                 editor = "/usr/bin/vim";            
             }
+            /* TODO
+             * test: just open vim and let it handle the file */
             return execl(editor, editor, "/etc/test.conf", NULL);
        }
 
@@ -145,16 +162,12 @@ int main (int argc, char *argv[]) {
 		if (argc >= 4) {
             usage("Too many arguments!\n");
          }
-	
 	} else {
 		usage("Unknown command!\n");
 	}
-	
 	econf_free(key_file);
 	return EXIT_SUCCESS;
 }
-
-
 
 static void usage(char *message) {
 	fprintf(stderr,"%s\n", message);
@@ -168,11 +181,3 @@ static void usage(char *message) {
 		"revert   reverts all changes to the vendor versions. Basically deletes the config files in /etc\n\n");
 	exit(EXIT_FAILURE);
 }
-
-
-
-
-
-
-
-
